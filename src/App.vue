155fb28735ff4f0e567a6ef35d1b0270c9312504
Emrah Fidan <201805070@stu.adu.edu.tr>
2024-01-05 18:52:26 +0300

    fixed errors

--- src/App.vue
+++ src/App.vue
@@ -12,43 +12,5 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-/* route transitions */
-.route-enter-from{
-  opacity: 0;
-  transform: translateY(100px);
-}
-.route-enter-active{
-  transition: all 0.5s ease-out;
-}
-
-.route-leave-to{
-  opacity: 0;
-  transform: translateY(-100px);
-}
-.route-leave-active{
-  transition: all 0.5s ease-in;
-}
-
-
+@import "../css/main.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// bu kısım ilk çalışan yerimiz ve burada id'si app olan div'e bağlanıyoruz
 createApp(App).use(store).use(router).mount('#app')
