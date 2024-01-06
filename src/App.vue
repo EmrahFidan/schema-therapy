4506f98c375c630127e4fa31c1d46caaea3060df
Emrah Fidan <201805070@stu.adu.edu.tr>
2024-01-06 15:28:26 +0300

    dark-light mode toggle added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/test">Test</router-link>
-  </nav>
-  
+  <div>
+    <nav>
+      <div class="links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/test">Test</router-link>
+      </div>
 
-  <router-view v-slot="{ Component }">
-    <transition name="route" mode="out-in">
-      <component :is="Component"></component>
-    </transition>
-  </router-view>
+      <div class="dark-light">
+        <label class="switch">
+          <input type="checkbox" id="darkmode-toggle" @change="switchTheme" />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </nav>
+
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <style>
 @import "../css/main.css";
 </style>
+
+<script>
+export default {
+  mounted() {
+    // Check user preference in local storage and set dark mode accordingly
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.getElementById("darkmode-toggle").checked = true;
+      this.switchTheme();
+    }
+  },
+  methods: {
+    switchTheme() {
+      const toggleSwitch = document.getElementById("darkmode-toggle");
+      if (toggleSwitch.checked) {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+    },
+  },
+};
+</script>
