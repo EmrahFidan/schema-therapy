c6e83214e2b362bb9766e512994d5180cad68685
begumıs <begum.is@outlook.com>
2024-01-06 15:31:41 +0300

    home page update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,54 @@
 <template>
-  <div class="home">
-    <h1>Hola fellas</h1>
+  <div class="home"></div>
+  <div class="container">
+    <div class="loader" style="--clr: #01e100; --i: 1">
+      <div class="card-content">
+        <p>
+          Sürekli size soğuk davranan kişilerle mi ilişkiler yaşıyorsunuz? En
+          yakın hissettiğiniz kişilerin bile sizi umursamadığını ya da yeterince
+          anlamadığım mı düşünüyorsunuz?
+        </p>
+      </div>
+    </div>
+    <div class="loader" style="--clr: #f50076; --i: 2">
+      <div class="card-content">
+        <p>
+          Aslında bir şekilde kusurlu olduğunuzu ve sizi gerçekten tanıyan
+          birinin sizi sevip kabullenmesinin olasılıklı olmadığını mı
+          düşünüyorsunuz?
+        </p>
+      </div>
+    </div>
+    <div class="loader" style="--clr: #00e6fd; --i: 3">
+      <div class="card-content">
+        <p>
+          Başkalarının ihtiyaçlarını kendinizinkilerden önde mi tutuyorsunuz?
+          Böylece ihtiyaçlarınıi hiç karşılanmıyor mu-hatta ihtiyaçlarınızın
+          neler olduğunu bile bilmiyor musunuz?
+        </p>
+      </div>
+    </div>
+  </div>
+  <br />
+  <div class="container1">
+    <div class="loader1" style="--clr: #a400fd; --i: 3">
+      <div class="card-content">
+        <div>
+          <h4>Şema Nedir</h4>
+          <p>
+            Şema, çocukluktan başlayan ve yaşam boyunca sürekli tekrar eden bir
+            kalıptır. Bize ailemiz ya da diğer çocuklar tarafindan yapılan bir
+            şey le başlamıştır. Terk edilmiş, eleştirilmiş, aşırı korunmuş,
+            istismar edil miş, yok sayılmış ya da yoksun bırakılmışızdır - bir
+            şekilde zarar gör müşüzdür. Sonunda şema hayatımızın bir parçası
+            olur. Büyüdüğümüz evden ayrıldıktan yıllar sonra bile, bize kötü
+            davranılan, ihmal edildiği miz, aşağılandığımız ya da kontrol
+            edildiğimiz ve istediğimiz hedefle re bir türlü ulaşamadığımız
+            durumlar yaratmaya devam ederiz.p>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
