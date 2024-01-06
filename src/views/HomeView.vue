67345da477e1ec85a5154ec1c404727404d4f85a
begumıs <begum.is@outlook.com>
2024-01-06 16:35:40 +0300

    home page updated 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
           <p>
             Sürekli size soğuk davranan kişilerle mi ilişkiler yaşıyorsunuz? En
             yakın hissettiğiniz kişilerin bile sizi umursamadığını ya da
-            yeterince anlamadığım mı düşünüyorsunuz?
+            yeterince anlamadığını mı düşünüyorsunuz?
           </p>
         </div>
       </div>
@@ -24,7 +24,7 @@
         <div class="card-content">
           <p>
             Başkalarının ihtiyaçlarını kendinizinkilerden önde mi tutuyorsunuz?
-            Böylece ihtiyaçlarınıi hiç karşılanmıyor mu-hatta ihtiyaçlarınızın
+            Böylece ihtiyaçlarını hiç karşılanmıyor mu-hatta ihtiyaçlarınızın
             neler olduğunu bile bilmiyor musunuz?
           </p>
         </div>
@@ -33,25 +33,25 @@
     <br />
     <div class="container1">
       <div class="loader1" style="--clr: #a400fd; --i: 3">
-        <div class="card-content1">
+        <div class="card-content">
           <div>
-            <h4>Şema Nedir ?</h4>
+            <h4>Şema Nedir?</h4>
             <p>
               Şema, çocukluktan başlayan ve yaşam boyunca sürekli tekrar eden
-              bir kalıptır. Bize ailemiz ya da diğer çocuklar tarafindan yapılan
-              bir şey le başlamıştır. Terk edilmiş, eleştirilmiş, aşırı
-              korunmuş, istismar edil miş, yok sayılmış ya da yoksun
+              bir kalıptır. Bize ailemiz ya da diğer çocuklar tarafından yapılan
+              bir şeyle başlamıştır. Terk edilmiş, eleştirilmiş, aşırı
+              korunmuş, istismar edilmiş, yok sayılmış ya da yoksun
               bırakılmışızdır - bir şekilde zarar görmüşüzdür. Sonunda şema
               hayatımızın bir parçası olur. Büyüdüğümüz evden ayrıldıktan yıllar
-              sonra bile, bize kötü davranılan, ihmal edildiği miz,
+              sonra bile, bize kötü davranılan, ihmal edildiğimiz miz,
               aşağılandığımız ya da kontrol edildiğimiz ve istediğimiz hedefle
-              re bir türlü ulaşamadığımız durumlar yaratmaya devam ederiz.
+              bir türlü ulaşamadığımız durumlar yaratmaya devam ederiz.
             </p>
           </div>
         </div>
       </div>
       <div class="loader1" style="--clr: #a400fd; --i: 3">
-        <div class="card-content1">
+        <div class="card-content">
           <div>
             <h4>Şema Terapi Nedir?</h4>
             <p>
@@ -60,54 +60,6 @@
               farkına varılması hedeflenir. Bu süreçte, terapist kişiye destek
               sağlar ve yeni ve sağlıklı düşünce ve davranış modelleri
               geliştirmesi konusunda rehberlik eder.
-            </p>
-          </div>
-  <div class="home">
-    <div class="container">
-      <div class="loader" style="--clr: #01e100; --i: 1">
-        <div class="card-content">
-          <p>
-            Sürekli size soğuk davranan kişilerle mi ilişkiler yaşıyorsunuz? En
-            yakın hissettiğiniz kişilerin bile sizi umursamadığını ya da
-            yeterince anlamadığım mı düşünüyorsunuz?
-          </p>
-        </div>
-      </div>
-      <div class="loader" style="--clr: #f50076; --i: 2">
-        <div class="card-content">
-          <p>
-            Aslında bir şekilde kusurlu olduğunuzu ve sizi gerçekten tanıyan
-            birinin sizi sevip kabullenmesinin olasılıklı olmadığını mı
-            düşünüyorsunuz?
-          </p>
-        </div>
-      </div>
-      <div class="loader" style="--clr: #00e6fd; --i: 3">
-        <div class="card-content">
-          <p>
-            Başkalarının ihtiyaçlarını kendinizinkilerden önde mi tutuyorsunuz?
-            Böylece ihtiyaçlarınıi hiç karşılanmıyor mu-hatta ihtiyaçlarınızın
-            neler olduğunu bile bilmiyor musunuz?
-          </p>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div class="container1">
-      <div class="loader1" style="--clr: #a400fd; --i: 3">
-        <div class="card-content">
-          <div>
-            <h4>Şema Nedir</h4>
-            <p>
-              Şema, çocukluktan başlayan ve yaşam boyunca sürekli tekrar eden
-              bir kalıptır. Bize ailemiz ya da diğer çocuklar tarafindan yapılan
-              bir şey le başlamıştır. Terk edilmiş, eleştirilmiş, aşırı
-              korunmuş, istismar edil miş, yok sayılmış ya da yoksun
-              bırakılmışızdır - bir şekilde zarar gör müşüzdür. Sonunda şema
-              hayatımızın bir parçası olur. Büyüdüğümüz evden ayrıldıktan yıllar
-              sonra bile, bize kötü davranılan, ihmal edildiği miz,
-              aşağılandığımız ya da kontrol edildiğimiz ve istediğimiz hedefle
-              re bir türlü ulaşamadığımız durumlar yaratmaya devam ederiz.p>
             </p>
           </div>
         </div>
